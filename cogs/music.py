ce1eb1f579dedd771bbae48fbc623e7b60424b0d
bowtiesarecool26 <varun.j@learner.manipal.edu>
2020-09-26 15:39:26 +0000

    bleh

--- cogs/music.py
+++ cogs/music.py
@@ -51,13 +51,13 @@ class Music(commands.Cog):
         connection = await self.client.db.acquire()
         async with connection.transaction():
             if await self.client.db.fetchrow("SELECT * FROM music WHERE guild_id = $1 and voice_id = $2", ctx.guild.id, voice.id):
-                await self.client.db.execute("UPDATE music SET text_id = $1 WHERE voice_id = $2 AND guild_id = $3", text.id, voice.id, guild_id)
+                await self.client.db.execute("UPDATE music SET text_id = $1 WHERE voice_id = $2 AND guild_id = $3", text.id, voice.id, ctx.guild.id)
             elif await self.client.db.fetchrow("SELECT * FROM music WHERE guild_id = $1 and text_id = $2", ctx.guild.id, text.id):
-                await self.client.db.execute("UPDATE music SET voice_id = $1 WHERE text_id = $2 AND guild_id = $3", voice.id, text.id, guild_id)
+                await self.client.db.execute("UPDATE music SET voice_id = $1 WHERE text_id = $2 AND guild_id = $3", voice.id, text.id, ctx.guild.id)
             else:
                 await self.client.db.execute("INSERT INTO music (guild_id, voice_id, text_id) VALUES ($1, $2, $3)", ctx.guild.id, voice.id, text.id)
         await self.client.db.release(connection)
-        await ctx.send(f"Mapped {voice.mention} with {text.mention}. Make sure to set permissions for {text.mentions} accordingly.")
+        await ctx.send(f"Mapped {voice.mention} with {text.mention}. Make sure to set permissions for {text.mention} accordingly.")
 
     @commands.command()
     @commands.guild_only()
