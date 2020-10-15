57645bc0a74a6f9ad4b25965f86ce28e2f7e70ed
darthshittious <varunj26012001@gmail.com>
2020-10-15 16:04:48 +0530

    Update funhouse to exclude duplicate command

--- cogs/funhouse.py
+++ cogs/funhouse.py
@@ -3,49 +3,11 @@ from discord.ext import commands
 import googletrans
 import io
 
-GUILD_ID = 718378271800033318
-NO_MIC_BOUNCE_ID = 758217695748554802
-BOUNCE_VOICE_ID = 718378272337166396
-INTRO_ID = 744131093568946229
-
 class Funhouse(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.trans = googletrans.Translator()
-
-    def is_in_bounce(self, state):
-        return state.channel is not None and state.channel.id == BOUNCE_VOICE_ID
-
-    def is_out_of_bounce(self, state):
-        return state.channel is None or state.channel.id != BOUNCE_VOICE_ID
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.guild and message.guild.id == GUILD_ID:
-            if message.channel == message.guild.get_channel(INTRO_ID):
-                if not message.author.guild_permissions.manage_guild:
-                    await message.channel.set_permissions(message.author, send_messages=False)
-
-    @commands.command(hidden=True)
-    @commands.guild_only()
-    async def resetintro(self, ctx, member: discord.Member):
-        if not ctx.author == ctx.guild.owner:
-            return
-        await ctx.guild.get_channel(INTRO_ID).set_permissions(member, send_messages=True)
-
-    @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, after):
-        if member.guild.id != GUILD_ID:
-            return
-
-        no_mic_bounce_channel = member.guild.get_channel(NO_MIC_BOUNCE_ID)
-        if self.is_out_of_bounce(before) and self.is_in_bounce(after):
-            # joined bounce
-            await no_mic_bounce_channel.set_permissions(member, read_messages=True)
-        elif self.is_in_bounce(before) and self.is_out_of_bounce(after):
-            # left bounce
-            await no_mic_bounce_channel.set_permissions(member, read_messages=None)
-
+        
     @commands.command(hidden=True)
     async def translate(self, ctx, *, message: commands.clean_content):
         """Translates a message to English using Google translate."""
