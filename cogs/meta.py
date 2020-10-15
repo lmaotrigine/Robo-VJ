abc6b0a6447e20a788244f7beb6dce206f6a99f9
Varun J <57328245+darthshittious@users.noreply.github.com>
2020-10-15 18:43:27 +0530

    Fix typo

--- cogs/meta.py
+++ cogs/meta.py
@@ -151,7 +151,7 @@ class Meta(commands.Cog):
                                 else:
                                     total += 1
                 msg = f'I am made of {total:,} lines of Python, spread across {file_amount:,} files!'
-                return await ctx.send('\n'.join(msg, source_url))
+                return await ctx.send('\n'.join((msg, source_url)))
 
         if command == 'help':
             src = type(self.bot.help_command)
