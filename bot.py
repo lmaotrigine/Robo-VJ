d0fbf2da2628a54d26330c4c1c375d51f6a81d75
darthshittious <varunj26012001@gmail.com>
2020-10-21 18:54:44 +0530

    Add tags

--- bot.py
+++ bot.py
@@ -159,6 +159,31 @@ class RoboVJ(commands.Bot):
             count INTEGER DEFAULT (1)
         );
         CREATE INDEX IF NOT EXISTS rtfm_user_id_idx ON rtfm (user_id);
+        CREATE TABLE IF NOT EXISTS tags (
+            id SERIAL PRIMARY KEY,
+            name TEXT,
+            content TEXT,
+            owner_id BIGINT,
+            uses INTEGER DEFAULT (0),
+            location_id BIGINT,
+            created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() at time zone 'utc')
+        );
+        CREATE INDEX IF NOT EXISTS tags_name_idx ON tags (name);
+        CREATE INDEX IF NOT EXISTS tags_location_id_idx ON tags (location_id);
+        CREATE INDEX IF NOT EXISTS tags_name_trgm_idx ON tags USING GIN (name gin_trgm_ops);
+        CREATE INDEX IF NOT EXISTS tags_name_lower_idx ON tags (LOWER(name));
+        CREATE UNIQUE INDEX IF NOT EXISTS tags_uniq_idx ON tags (LOWER(name), location_id);
+        CREATE TABLE IF NOT EXISTS tag_lookup (
+            id SERIAL PRIMARY KEY,
+            name TEXT, --index
+            location_id BIGINT, --index
+            owner_id BIGINT,
+            created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() at time zone 'utc'),
+            tag_id INTEGER REFERENCES tags (id) ON DELETE CASCADE ON UPDATE NO ACTION
+        );
+        CREATE INDEX IF NOT EXISTS tag_lookup_name_trgm_idx ON tag_lookup USING GIN (name gin_trgm_ops);
+        CREATE INDEX IF NOT EXISTS tag_lookup_name_lower_idx ON tag_lookup (LOWER(name));
+        CREATE UNIQUE INDEX IF NOT EXISTS tag_lookup_uniq_idx ON tag_lookup (LOWER(name), location_id);
         """)
 
     async def on_command_error(self, ctx, error):
