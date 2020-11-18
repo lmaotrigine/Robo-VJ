a1c6668161dc7656e440a8252e88cc0998620b0e
darthshittious <varunj26012001@gmail.com>
2020-11-18 22:48:24 +0530

    nnn

--- cogs/rtfx.py
+++ cogs/rtfx.py
@@ -10,8 +10,9 @@ import discord
 from discord.ext import commands
 from .utils import fuzzy
 
+
 class SphinxObjectFileReader:
-    """A Sphinx file reader."""
+    """ A Sphinx file reader. """
     # Inspired by Sphinx's InventoryFileReader
     BUFSIZE = 16 * 1024
 
@@ -43,9 +44,8 @@ class SphinxObjectFileReader:
                 buf = buf[pos + 1:]
                 pos = buf.find(b'\n')
 
-class RTFX(commands.Cog):
-    """Read the f*cking docs/source."""
 
+class RTFX(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -61,17 +61,19 @@ class RTFX(commands.Cog):
             raise RuntimeError('Invalid objects.inv file version.')
 
         # next line is "# Project: <name>"
-        # then after that is "# version: <version>"
+        # then after that is "# Version: <version>"
         projname = stream.readline().rstrip()[11:]
         version = stream.readline().rstrip()[11:]
 
         # next line says if it's a zlib header
         line = stream.readline()
         if 'zlib' not in line:
-            raise RuntimeError('Invalid objects.inv file. Not zlib compatible.')
+            raise RuntimeError(
+                'Invalid objects.inv file, not z-lib compatible.')
 
-        # this code comes from the Sphinx repository.
-        entry_regex = re.compile(r'(?x)(.+?)\s+(\S*:\S*)\s+(-?\d+)\s+(\S+)\s+(.*)')
+        # This code mostly comes from the Sphinx repository.
+        entry_regex = re.compile(
+            r'(?x)(.+?)\s+(\S*:\S*)\s+(-?\d+)\s+(\S+)\s+(.*)')
         for line in stream.read_compressed_lines():
             match = entry_regex.match(line.rstrip())
             if not match:
@@ -80,35 +82,40 @@ class RTFX(commands.Cog):
             name, directive, _, location, dispname = match.groups()
             domain, _, subdirective = directive.partition(':')
             if directive == 'py:module' and name in result:
-                # From the Sphinx respository
-                # due to a bug in 1.1. and below,
+                # From the Sphinx Repository:
+                # due to a bug in 1.1 and below,
                 # two inventory entries are created
                 # for Python modules, and the first
-                # one is correct.
+                # one is correct
                 continue
 
             # Most documentation pages have a label
             if directive == 'std:doc':
                 subdirective = 'label'
-            
+
             if location.endswith('$'):
                 location = location[:-1] + name
-            
+
             key = name if dispname == '-' else dispname
             prefix = f'{subdirective}:' if domain == 'std' else ''
 
             if projname == 'discord.py':
-                key = key.replace('discord.ext.commands.', '').replace('discord.', '')
-            
+                key = key.replace('discord.ext.commands.',
+                                  '').replace('discord.', '')
+
             result[f'{prefix}{key}'] = os.path.join(url, location)
+
         return result
 
     async def build_rtfm_lookup_table(self, page_types):
         cache = {}
         for key, page in page_types.items():
+            # sub = cache[key] = {}
             async with self.bot.session.get(page + '/objects.inv') as resp:
                 if resp.status != 200:
-                    raise RuntimeError('Cannot build rtfm lookup table, try again later.')
+                    raise RuntimeError(
+                        'Cannot build rtfm lookup table, try again later.')
+
                 stream = SphinxObjectFileReader(await resp.read())
                 cache[key] = self.parse_object_inv(stream, page)
 
@@ -116,12 +123,14 @@ class RTFX(commands.Cog):
 
     async def do_rtfm(self, ctx, key, obj):
         page_types = {
-            'discord.py': 'https://discordpy.readthedocs.io/en/latest',
+            'discord.py': 'https://discordpy.readthedocs.io/en/latest/',
+            'discord.py-jp': 'https://discordpy.readthedocs.io/ja/latest/',
             'python': 'https://docs.python.org/3',
-            'asyncpg': 'https://magicstack.githiub.io/asyncpg/current',
-            'twitchio': 'https://twitchio.readthedocs.io/en/rewrite',
-            'aiohttp': 'https://docs.aiohttp.org/en/stable',
-            'wavelink': 'https://wavelink.readthedocs.io/en/latest',
+            'python-jp': 'https://docs.python.org/ja/3',
+            'asyncpg': 'https://magicstack.github.io/asyncpg/current/',
+            'twitchio': 'https://twitchio.readthedocs.io/en/rewrite/',
+            'aiohttp': 'https://docs.aiohttp.org/en/stable/',
+            'wavelink': 'https://wavelink.readthedocs.io/en/latest/'
         }
 
         if obj is None:
@@ -132,7 +141,8 @@ class RTFX(commands.Cog):
             await ctx.trigger_typing()
             await self.build_rtfm_lookup_table(page_types)
 
-        obj = re.sub(r'^(?:discord\.(?:ext\.)?)?(?:commands\.)?(.+)', r'\1', obj)
+        obj = re.sub(
+            r'^(?:discord\.(?:ext\.)?)?(?:commands\.)?(.+)', r'\1', obj)
 
         if key.startswith('discord.'):
             # point the abc.Messageable types properly:
@@ -148,66 +158,99 @@ class RTFX(commands.Cog):
 
         matches = fuzzy.finder(obj, cache, key=lambda t: t[0], lazy=False)
 
-        embed = discord.Embed(colour=discord.Colour.blurple())
+        e = discord.Embed(colour=self.bot.colour['dsc'])
         if not matches:
             return await ctx.send('Could not find anything. Sorry.')
-        embed.title = f'RTFM for __**`{key}`**__: {obj}'
-        embed.description = '\n'.join(f'[`{key}`]({url})' for key, url in matches[:8])
-        embed.set_footer(text=f'{len(matches)} possible results.')
-        await ctx.send(embed=embed)
+        e.title = f"RTFM for __**`{key}`**__: {obj}"
+        e.description = '\n'.join(f'[`{key}`]({url})' for key, url in matches[:8])
+        e.set_footer(text=f"{len(matches)} possible results.")
+        await ctx.send(embed=e)
 
     @commands.group(aliases=['rtfd'], invoke_without_command=True)
-    async def rtfm(self, ctx, *, obj: str=None):
+    async def rtfm(self, ctx, *, obj: str = None):
         """Gives you a documentation link for a discord.py entity.
-
         Events, objects, and functions are all supported through a
-        cruddy fuzzy algorithm.
+        a cruddy fuzzy algorithm.
         """
-        await self.do_rtfm(ctx, 'discord.py', obj)
+        await self.do_rtfm(ctx, "discord.py", obj)
+
+    @rtfm.command(name='jp')
+    async def rtfm_jp(self, ctx, *, obj: str = None):
+        """Gives you a documentation link for a discord.py entity (Japanese)."""
+        await self.do_rtfm(ctx, 'discord.py-jp', obj)
 
     @rtfm.command(name='python', aliases=['py'])
-    async def rtfm_python(self, ctx, *, obj: str=None):
+    async def rtfm_python(self, ctx, *, obj: str = None):
         """Gives you a documentation link for a Python entity."""
-        await self.do_rtfm(ctx, 'python', obj)
+        await self.do_rtfm(ctx, "python", obj)
 
-    @rtfm.command(name='asyncpg')
-    async def rtfm_asyncpg(self, ctx, *, obj: str=None):
-        """Gives you a documentation link for an `asyncpg` entity."""
+    @rtfm.command(name='py-jp', aliases=['py-ja'])
+    async def rtfm_python_jp(self, ctx, *, obj: str = None):
+        """Gives you a documentation link for a Python entity (Japanese)."""
+        await self.do_rtfm(ctx, 'python-jp', obj)
+
+    @rtfm.command(name="asyncpg")
+    async def rtfm_asyncpg(self, ctx, *, obj: str = None):
+        """ Gives you the documentation link for an `asyncpg` entity. """
         await self.do_rtfm(ctx, 'asyncpg', obj)
 
-    @rtfm.command(name='twitchio')
-    async def rtfm_twitchio(self, ctx, *, obj: str=None):
-        """Gives you a focumentation link for a `twitchio` entity."""
+    @rtfm.command(name="twitchio")
+    async def rtfm_twitchio(self, ctx, *, obj: str = None):
+        """ Gives you the documentation link for a `twitchio` entity. """
         await self.do_rtfm(ctx, 'twitchio', obj)
 
-    @rtfm.command(name='aiohttp')
-    async def rtfm_aiohttp(self, ctx, *, obj: str=None):
-        """Gives you a documentation link for an `aiohttp` entity."""
+    @rtfm.command(name="aiohttp")
+    async def rtfm_aiohttp(self, ctx, *, obj: str = None):
+        """ Gives you the documentation link for an `aiohttp` entity. """
         await self.do_rtfm(ctx, 'aiohttp', obj)
 
-    @rtfm.command(name='wavelink')
-    async def rtfm_wavelink(self, ctx, *, obj: str=None):
-        """Gives you a documentation link for a `Wavelink` entity."""
+    @rtfm.command(name="wavelink")
+    async def rtfm_wavelink(self, ctx, *, obj: str = None):
+        """ Gives you the documentation link for a `Wavelink` entity. """
         await self.do_rtfm(ctx, 'wavelink', obj)
+
+    async def _member_stats(self, ctx, member, total_uses):
+        e = discord.Embed(title='RTFM Stats')
+        e.set_author(name=str(member), icon_url=member.avatar_url)
+
+        query = 'SELECT count FROM rtfm WHERE user_id=$1;'
+        record = await ctx.db.fetchrow(query, member.id)
+
+        if record is None:
+            count = 0
+        else:
+            count = record['count']
+
+        e.add_field(name='Uses', value=count)
+        e.add_field(name='Percentage',
+                    value=f'{count/total_uses:.2%} out of {total_uses}')
+        e.colour = self.bot.colour['dsc']
+        await ctx.send(embed=e)
 
     @commands.command()
     async def rtfs(self, ctx, *, search: str):
-        """Read the fuckin' source of discord.py."""
-        embed = discord.Embed(title='Read the f*ckin source', colour=discord.Colour.blurple())
+        """ Read the fuckin' source of discord.py. """
+        embed = discord.Embed(
+            title="Read the f*ckin source",
+            colour=self.bot.colour['dsc']
+        )
         timeout = ClientTimeout(5)
         try:
-            async with self.bot.session.get(f'https://rtfs.eviee.me/dpy?search={search}', timeout=timeout) as resp:
+            async with self.bot.session.get(f"https://rtfs.eviee.me/dpy?search={search}", timeout=timeout) as resp:
                 results = await resp.json()
         except TimeoutError:
-            return await ctx.send('API is down, go look yourself lmao: <https://github.com/Rapptz/discord.py>')
+            return await ctx.send("API is down, go look yourself lmao: <https://github.com/Rapptz/discord.py>")
         if not results:
             embed.title = "Couldn't find anything."
         else:
             embed.title = f"RTFS for '{search}'"
-            embed.description = '\n'.join(f"[`{result['module']}.{result['object']}`]({result['url']})" for result in results[:10])
+            embed.description = '\n'.join(
+                f"[`{result['module']}.{result['object']}`]({result['url']})" for result in results[:10])
             eviee = self.bot.get_user(402159684724719617) or "Eviee#0666"  # just in case ;q
-            embed.set_footer(text=f'Requested by {ctx.author} | Thank you {eviee} for the API.')
-        await ctx.send(embed=embed)
+            embed.set_footer(
+                text=f"Requested by {ctx.author} | Thank you {eviee} for the API.")
+        return await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(RTFX(bot))
