1e2e551ba66a6f462b3b6150cf5e1f0f7fb2b278
darthshittious <varunj26012001@gmail.com>
2020-12-27 15:26:49 +0530

    Update my_quiz.py

--- cogs/my_quiz.py
+++ cogs/my_quiz.py
@@ -57,7 +57,7 @@ class PubQuiz(commands.Cog, name="Pub Quiz"):
         except:
             pass
         try:
-            self.wh = discord.utils.get(self.bot.get_guild(GUILD_ID).get_channel(MESSAGE_LOGS).webhooks,
+            self.wh = discord.utils.get((await self.bot.get_guild(GUILD_ID).get_channel(MESSAGE_LOGS).webhooks()),
                                         user=self.bot.user)
         except AttributeError:
             self.wh = None
