e582e271e0a00cd8e0f6d2feec41cfb599ce2ffe
darthshittious <varunj26012001@gmail.com>
2020-12-29 16:22:37 +0530

    Make aliases functions

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -1,4 +1,21 @@
-alias venv="python3.9 -m venv venv && . venv/bin/activate && pip install -U pip setuptools wheel &> /dev/null"
-alias yeet="git push"
-alias yoink="git pull"
-alias please="sudo"
+function venv {
+  python3.9 -m venv venv
+  . venv/bin/activate
+  pip install -U pip setuptools wheel &> /dev/null
+}
+export -f venv
+
+function please {
+  sudo "$@"
+}
+export -f please
+
+function yoink {
+  git pull "$@"
+}
+export -f yoink
+
+function yeet {
+  git push "$@"
+}
+export -f yeet
