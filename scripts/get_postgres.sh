ab9c0503deb3cee8f4f5f21fbbf34b44c15f6d54
darthshittious <varunj26012001@gmail.com>
2020-12-29 20:16:09 +0530

    Fix broken postgres install

--- scripts/get_postgres.sh
+++ scripts/get_postgres.sh
@@ -4,7 +4,7 @@
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 # Import the repository signing key:
-wget --quiet -O https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 # Update the package lists:
 sudo apt-get update
