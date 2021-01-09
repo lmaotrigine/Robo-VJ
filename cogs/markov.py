f16b59725f98f7f3187a3c69c197d44016d73b48
darthshittious <varunj26012001@gmail.com>
2021-01-09 20:28:35 +0530

    Error handling

--- cogs/markov.py
+++ cogs/markov.py
@@ -230,6 +230,10 @@ class Markov(commands.Cog):
             model = await self.get_model(args, coro, order=3)
         await self.send_markov(ctx, model, 3, seed=seed.lower())
 
+    def cog_command_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send(error)
+
 
 def setup(bot):
     bot.add_cog(Markov(bot))
