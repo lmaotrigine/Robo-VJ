815ae8bee7a15e90b3389f022a79d632dcc9e5e0
darthshittious <varunj26012001@gmail.com>
2021-01-18 13:41:02 +0530

    DBL webhook

--- cogs/dbl.py
+++ cogs/dbl.py
@@ -9,6 +9,7 @@ log = logging.getLogger(__name__)
 
 
 DISCORD_BOTS_API = 'https://discord.bots.gg/api/v1'
+TOP_GG_API = 'https://top.gg/api'
 
 
 class DBL(commands.Cog):
@@ -37,11 +38,18 @@ class DBL(commands.Cog):
         url = f'{DISCORD_BOTS_API}/bots/{self.bot.user.id}/stats'
         async with self.bot.session.post(url, data=payload, headers=headers) as resp:
             log.info(f'DBots statistics returned {resp.status} for {payload}')
-        try:
-            await self.dbl_client.post_guild_count(guild_count)
-            log.info(f'DBL statistics posted successfully.')
-        except Exception as e:
-            log.info(f'DBL Statistics post failed:\n{e.__class__.__name__}: {e}')
+
+        headers = {
+            'Authorization': self.bot.config.dbl_token,
+            'Content-Type': 'application/json'
+        }
+        payload = json.dumps({
+            'server_count': guild_count,
+            'shard_count': len(self.bot.shards)
+        })
+        url = f'{TOP_GG_API}/bots/{self.bot.user.id}/stats'
+        async with self.bot.session.post(url, data=payload, headers=headers) as resp:
+            log.info(f'Top.gg statistics returned {resp.status} for {payload}.')
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
