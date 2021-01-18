d96527c6dcd45fcb2ca5d093afb8722be67cbc95
darthshittious <varunj26012001@gmail.com>
2021-01-18 11:57:16 +0530

    DBL webhook

--- cogs/dbl.py
+++ cogs/dbl.py
@@ -1,6 +1,9 @@
+import discord
 from discord.ext import commands
 import json
 import logging
+import dbl
+import datetime
 
 log = logging.getLogger(__name__)
 
@@ -13,6 +16,10 @@ class DBL(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.dbl_client = dbl.DBLClient(self.bot, self.bot.config.dbl_token, autopost=False,
+                                        webhook_port=5000, webhook_auth=self.bot.config.dbl_auth)
+        self.webhook = discord.Webhook.partial(*self.bot.config.dbl_webhook,
+                                               adapter=discord.AsyncWebhookAdapter(session=self.bot.session))
 
     async def update(self):
         guild_count = len(self.bot.guilds)
@@ -30,6 +37,11 @@ class DBL(commands.Cog):
         url = f'{DISCORD_BOTS_API}/bots/{self.bot.user.id}/stats'
         async with self.bot.session.post(url, data=payload, headers=headers) as resp:
             log.info(f'DBots statistics returned {resp.status} for {payload}')
+        try:
+            await self.dbl_client.post_guild_count(guild_count)
+            log.info(f'DBL statistics posted successfully.')
+        except Exception as e:
+            log.info(f'DBL Statistics post failed:\n{e.__class__.__name__}: {e}')
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
@@ -42,6 +54,22 @@ class DBL(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         await self.update()
+
+    @commands.Cog.listener()
+    async def on_dbl_vote(self, data):
+        user_id = data.get('user')
+        user = self.bot.get_user(user_id)
+        if user is None:
+            user = await self.bot.fetch_user(user_id)
+        embed = discord.Embed(title='New upvote', colour=discord.Colour.blurple())
+        embed.set_author(name=str(user), icon_url=user.avatar_url)
+        embed.add_field(name='Total Votes', value=(await self.dbl_client.get_bot_info())['points'])
+        embed.timestamp = datetime.datetime.utcnow()
+        await self.webhook.send(embed=embed, avatar_url='https://top.gg/images/dblnew.png', username='Top.gg')
+
+    @commands.Cog.listener()
+    async def on_dbl_test(self, data):
+        await self.webhook.send(str(data))
 
 
 def setup(bot):
