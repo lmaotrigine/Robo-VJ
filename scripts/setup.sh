6cbfe00119a4cbd691e930649da26762d58cf9d7
darthshittious <varunj26012001@gmail.com>
2021-01-27 20:03:14 +0530

    Fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,14 +6,17 @@ if test -f "$HOME/config.py"; then
     mv $HOME/config.py ../
 fi
 
-cd ..
 git config --global credential.helper store
 git config --global user.name darthshittious
 git config --global user.email varunj26012001@gmail.com
 
 sudo apt update && sudo apt upgrade -y
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.9 python3.9-venv python3.9-dev libjpeg-dev libtiff-dev
+sudo apt install python3.9 python3.9-venv python3.9-dev libjpeg-dev libtiff-dev libcairo2-dev
+
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+
 if [[ -n $SSH_CONNECTION ]] ; then
     bash -i ./get_postgres.sh
     sudo cp bot.service /etc/systemd/system/bot.service
