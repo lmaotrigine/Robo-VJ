9f3b8f230a2215585d46ae43fd84ddbe942271f6
darthshittious <varunj26012001@gmail.com>
2021-02-01 10:49:51 +0530

    Actually print out stuff

--- cogs/my_quiz.py
+++ cogs/my_quiz.py
@@ -456,7 +456,7 @@ class PubQuiz(commands.Cog, name="The Red Lion"):
         voice = ctx.guild.get_channel(BOUNCE_VOICE_ID)
         solo = ctx.guild.get_role(SOLO_PARTICIPANT_ROLE)
         embed = discord.Embed()
-        embed.set_footer(text=f'Use `{ctx.prefix}voicelist` for more details.')
+        embed.add_field(name='\u200b', value=f'Use `{ctx.prefix}voicelist` for more details.', inline=False)
         if solo.members:
             inside = sum(m in voice.members for m in solo.members)
             embed.description = f'`{inside}/{solo.members}` participants in voice.'
@@ -468,7 +468,8 @@ class PubQuiz(commands.Cog, name="The Red Lion"):
             inside = sum(m in voice.members for m in team.members)
             total += len(team.members)
             out.append(f'{team.mention}: {inside}/{team.members} members in voice.')
-        embed.add_field(name='Total', value=f'{len(voice.members)}/{total} members in voice.')
+        embed.description = '\n'.join(out)
+        embed.insert_field_at(0, name='Total', value=f'{len(voice.members)}/{total} members in voice.', inline=False)
         await ctx.send(embed=embed)
 
     @commands.command(hidden=True)
